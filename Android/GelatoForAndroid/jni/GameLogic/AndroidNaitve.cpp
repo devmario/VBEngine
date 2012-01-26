475d05ec1d3361866a9369d64c0f5d1272476af4
Changshik Jang <lowmans@vanillabreeze.com>
2012-01-26 17:56:54 +0900

    Android GelatoTest에 Assets 데이터 테스트완료

--- Android/GelatoForAndroid/jni/GameLogic/AndroidNaitve.cpp
+++ Android/GelatoForAndroid/jni/GameLogic/AndroidNaitve.cpp
@@ -7,12 +7,13 @@
 #include "../VBEngine/VBEngine.h"
 
 #include <android/log.h>
-#define LOGV(...) __android_log_print(ANDROID_LOG_VERBOSE, "libnav", __VA_ARGS__)
-#define LOGD(...) __android_log_print(ANDROID_LOG_DEBUG  , "libnav", __VA_ARGS__)
-#define LOGI(...) __android_log_print(ANDROID_LOG_INFO   , "libnav", __VA_ARGS__)
-#define LOGW(...) __android_log_print(ANDROID_LOG_WARN   , "libnav", __VA_ARGS__)
-#define LOGE(...) __android_log_print(ANDROID_LOG_ERROR  , "libnav", __VA_ARGS__)
 #define LOG_TAG  "VBEngine Test"
+#define LOGV(...) __android_log_print(ANDROID_LOG_VERBOSE, LOG_TAG, __VA_ARGS__)
+#define LOGD(...) __android_log_print(ANDROID_LOG_DEBUG  , LOG_TAG, __VA_ARGS__)
+#define LOGI(...) __android_log_print(ANDROID_LOG_INFO   , LOG_TAG, __VA_ARGS__)
+#define LOGW(...) __android_log_print(ANDROID_LOG_WARN   , LOG_TAG, __VA_ARGS__)
+#define LOGE(...) __android_log_print(ANDROID_LOG_ERROR  , LOG_TAG, __VA_ARGS__)
+
 
 
 static long prev_time;
