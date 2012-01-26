b71e34d91a4f55f929ca2654e406ca337d979bd6
Changshik Jang <lowmans@vanillabreeze.com>
2012-01-26 18:07:22 +0900

    Android GelatoTest 에 불필요한 파일제거

--- Android/GelatoForAndroid/obj/local/armeabi/objs-debug/vanillaworld/Other/zlib/compress.o.d
+++
@@ -1,7 +0,0 @@
-obj/local/armeabi/objs-debug/vanillaworld/Other/zlib/compress.o: \
- jni/VBEngine/Other/zlib/compress.c jni/VBEngine/Other/zlib/zlib.h \
- jni/VBEngine/Other/zlib/zconf.h
-
-jni/VBEngine/Other/zlib/zlib.h:
-
-jni/VBEngine/Other/zlib/zconf.h:
--- Android/GelatoForAndroid/obj/local/armeabi/objs-debug/vanillaworld/Other/zlib/uncompr.o.d
+++
@@ -1,7 +0,0 @@
-obj/local/armeabi/objs-debug/vanillaworld/Other/zlib/uncompr.o: \
- jni/VBEngine/Other/zlib/uncompr.c jni/VBEngine/Other/zlib/zlib.h \
- jni/VBEngine/Other/zlib/zconf.h
-
-jni/VBEngine/Other/zlib/zlib.h:
-
-jni/VBEngine/Other/zlib/zconf.h:
