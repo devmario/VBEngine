cc153f8e6146c41c3e7efbfa7c08a0e0375f95d5
Changshik Jang <lowmans@vanillabreeze.com>
2012-01-27 09:50:12 +0900

    Android OldVersion AndroidNative 태그수정

--- Android/OldVersion/VBEngineTest/jni/Native/AndroidNaitve.cpp
+++ Android/OldVersion/VBEngineTest/jni/Native/AndroidNaitve.cpp
@@ -7,12 +7,12 @@
 #include "../VanillaWorld/VBEngine.h"
 
 #include <android/log.h>
-#define LOGV(...) __android_log_print(ANDROID_LOG_VERBOSE, "libnav", __VA_ARGS__)
-#define LOGD(...) __android_log_print(ANDROID_LOG_DEBUG  , "libnav", __VA_ARGS__)
-#define LOGI(...) __android_log_print(ANDROID_LOG_INFO   , "libnav", __VA_ARGS__)
-#define LOGW(...) __android_log_print(ANDROID_LOG_WARN   , "libnav", __VA_ARGS__)
-#define LOGE(...) __android_log_print(ANDROID_LOG_ERROR  , "libnav", __VA_ARGS__)
-#define LOG_TAG  "VBEngine Test"
+#define LOG_TAG  "VBEngineTest"
+#define LOGV(...) __android_log_print(ANDROID_LOG_VERBOSE, LOG_TAG, __VA_ARGS__)
+#define LOGD(...) __android_log_print(ANDROID_LOG_DEBUG  , LOG_TAG, __VA_ARGS__)
+#define LOGI(...) __android_log_print(ANDROID_LOG_INFO   , LOG_TAG, __VA_ARGS__)
+#define LOGW(...) __android_log_print(ANDROID_LOG_WARN   , LOG_TAG, __VA_ARGS__)
+#define LOGE(...) __android_log_print(ANDROID_LOG_ERROR  , LOG_TAG, __VA_ARGS__)
 
 #include "EngineSample.h"
 
