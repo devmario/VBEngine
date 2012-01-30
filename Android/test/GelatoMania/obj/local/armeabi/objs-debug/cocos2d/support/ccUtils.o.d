9c17ba4a3674b59a1933761865ec9b479b8d2ee8
NaYounKyu <nyk0806@vanillabreeze.com>
2012-01-30 18:10:41 +0900

    sync

--- Android/test/GelatoMania/obj/local/armeabi/objs-debug/cocos2d/support/ccUtils.o.d
+++
@@ -1,4 +0,0 @@
-obj/local/armeabi/objs-debug/cocos2d/support/ccUtils.o: \
- jni/Cocos2dx/support/ccUtils.cpp jni/Cocos2dx/support/ccUtils.h
-
-jni/Cocos2dx/support/ccUtils.h:
--- Android/test/GelatoMania/obj/local/armeabi/objs-debug/vanillaworld/Other/png/pnggccrd.o.d
+++
@@ -1,2 +0,0 @@
-obj/local/armeabi/objs-debug/vanillaworld/Other/png/pnggccrd.o: \
- jni/VBEngine/Other/png/pnggccrd.c
