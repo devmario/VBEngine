c6fea029b9f7fe0beaed3f1031642c8a08c46e81
nyk0806 <nyk0806@vanillabreeze.com>
2012-02-22 18:36:29 +0900

    [안드로이드] 오토빌드 툴수정(mk파일들)

--- Android/cocos2d-vb-1.0.1-x-0.9.1/libs/Android.mk
+++ Android/cocos2d-vb-1.0.1-x-0.9.1/libs/Android.mk
@@ -1,3 +1,8 @@
 
 LOCAL_PATH := $(call my-dir)
-include $(call all-subdir-makefiles)
+# include $(call all-subdir-makefiles)
+
+subdirs := $(LOCAL_PATH)/VBEngine/Android.mk \
+           $(LOCAL_PATH)/GameLogic/Android.mk
+           
+include $(subdirs)
--- Android/cocos2d-vb-1.0.1-x-0.9.1/libs/Application.mk
+++ Android/cocos2d-vb-1.0.1-x-0.9.1/libs/Application.mk
@@ -9,4 +9,5 @@ APP_STL := stlport_shared
 APP_CPPFLAGS += -frtti
 # APP_ABI := armeabi armeabi-v7a
 # APP_PLATFORM := android-8
-APP_MODULES := cocos2d cocosdenshion vanillaworld gamelogic 
+# APP_MODULES := cocos2d cocosdenshion vanillaworld gamelogic 
+APP_MODULES := vanillaworld gamelogic
